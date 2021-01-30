ed025c6ddab6136d5919fe02ab5f80e118c93e76
fedeghe <fedeghe@gmail.com>
2021-01-30 21:23:24 +0100

    👍

--- source/Comp.jsx
+++ source/Comp.jsx
@@ -1,10 +1,49 @@
 import React, {useState} from 'react'
 import Channeljs from '@fedeghe/channeljs'
+import { uniqueID } from './utils'
+import uploader from './uploader'
+
+
+const url = 'http://localhost:3000/upload'
 
 export default ({name}) => {
-    const [n, setN] = useState(name)
-    Channeljs.get('java').sub('message', data => {
-        setN(data)
-    })
-    return <p>{n}</p>
+    const [uploads, setUploads] = useState([])
+    return <div>
+        <p>{name}</p>
+        <input type="file" name="myfile" multiple onChange={e => {
+            const files = [...e.target.files]
+            const ids = files.map(file => uploader.start({
+                method:'PUT',
+                file,
+                url: `${url}?fileName=${file.name}`,
+                onStart: data => {
+                    setUploads(old => {
+                        return [...old, data]
+                    })
+                    console.log('start')
+                    console.log(data)
+                    console.log('========')
+                },
+                onProgress: data => {
+                    console.log('progress')
+                    console.log(data)
+                    console.log('========')
+                },
+                onEnd: data => {
+                    console.log('end')
+                    console.log(data)
+                    console.log('========')
+                }
+            }))
+            console.log(ids)
+        }}/>
+        {uploads.length ? (
+            <ul>
+                {uploads.map(upload => (
+                    <li key={`${uniqueID}`}>{JSON.stringify(upload, null, 2)}</li>
+                ))}
+            </ul>
+        ) : null}
+        
+    </div>
 }
